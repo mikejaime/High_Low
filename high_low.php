2684ef5115005939286b46f7132b47bdb8d710cb
Mike Jaime <mikejaime@yahoo.com>
2014-05-12 10:34:06 -0500

    initial commit

---
+++ high_low.php
@@ -0,0 +1,25 @@
+<?php 
+
+/*
+// Write the output
+// Notice the space after the ?
+fwrite(STDOUT, 'What is your first name? ');
+
+// Get the input from user
+$first_name = fgets(STDIN);
+
+// Output the user's name
+fwrite(STDOUT, "Hello $first_name\n");
+*/
+
+
+/*
+The specs for the game are:
+- game picks a random number between 1 and 100.
+- prompts user to guess the number
+- if user's guess is less than the number, it outputs "HIGHER"
+- if user's guess is more than the number, it outputs "LOWER"
+- if a user guesses the number, the game should declare "GOOD GUESS!"
+*/
+
+ ?>
