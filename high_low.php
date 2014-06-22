00a2459521ad906d8aeeb2fe6cc56bc1196273e3
Mike Jaime <mikejaime@yahoo.com>
2014-06-22 01:53:09 -0500

    minor formatting and modified notes

--- high_low.php
+++ high_low.php
@@ -1,39 +1,36 @@
 <?php 
 
+// user prompted to set high & low numeric parameters
 if (!is_numeric($argv[1]) || !is_numeric($argv[2])) {
 	echo "You must input 2 numbers (a low & high numerical/number)\n";
 	exit(1);
 }
 
-//define high and low variable values
+// define high and low variable values
 // in this version the values are input via argv values in the terminal
 // if argv[1] number is higher than argv[2], then argvs' are reverseds
 if ($argv[1] > $argv[2]) {
 	$lownum = $argv[2];
 	$highnum = $argv[1];
-}
-else {
+} else {
 	$lownum = $argv[1];
 	$highnum = $argv[2];
 }
 
 
-
-
-//have comp generate a random number
+// have comp generate a random number
 $comp_pick = rand ($lownum, $highnum);
 
-//attempts counter
+// attempts counter
 $attempts = 0;
 
-//user prompted to guess a number
+// user prompted to guess a number
 fwrite(STDOUT, "Guess a number between $lownum & $highnum\n");
 
-
 do {
-	// Get the input from user, placed before comparion to avoid repition
+	// user input assigned to variable
 	$guess = fgets(STDIN);
-	//increment attempts counter, placed before comparison to avoid repition
+	// increment attempts counter
 	$attempts++;
 
 	if ($guess < $comp_pick) {
